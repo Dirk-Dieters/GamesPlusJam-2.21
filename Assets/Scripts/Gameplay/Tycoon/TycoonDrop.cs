7f127a9f8bb1c2386f1308fe38c79d93ac48ffee
Kevin Moore <zyronn.sorrow@gmail.com>
2021-02-19 22:12:23 -0700

    added tycoon scripts and set up tycoon testing scene

---
+++ Assets/Scripts/Gameplay/Tycoon/TycoonDrop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TycoonDrop : MonoBehaviour
+{
+	#region Components
+	private Rigidbody rb;
+	#endregion
+
+	#region MonoBehaviour
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void OnEnable()
+	{
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+	}
+	#endregion
+
+	#region Public Methods
+	public void Collect()
+	{
+		gameObject.SetActive(false);
+	}
+	#endregion
+}
