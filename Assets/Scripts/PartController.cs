67469d3b2a3fbb07221386ccfd97aecf71b5e11b
Dirk Dieters <35855037+Dirk-Dieters@users.noreply.github.com>
2021-02-22 13:30:02 -0700

    Vehicle Damage partially implemented

---
+++ Assets/Scripts/PartController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartController : MonoBehaviour
+{
+    public bool destroyed = false;
+    Transform attachment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attachment = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!destroyed)
+        {
+            transform.position = attachment.position;
+            transform.rotation = attachment.rotation;
+        }
+    }
+}
