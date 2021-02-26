088940a50f3403457e09e0e8a64a9da08b9b12e4
Kevin Moore <zyronn.sorrow@gmail.com>
2021-02-25 18:14:41 -0700

    core tycoon loop is working

---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+	#region Private Variables
+	private static float money;
+	#endregion
+
+	#region Properties
+	public static float Money { get { return money; } }
+	#endregion
+
+	#region Public Methods
+	public static void SetMoney(float value)
+	{
+		money = value;
+	}
+
+	public static void AddMoney(float value)
+	{
+		money += value;
+	}
+	#endregion
+}
